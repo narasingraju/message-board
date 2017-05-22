7158c7dd862dc2e05613c0f1fd9e11f72f6946fe
Narasinga Raju <raju.narasing20@gmail.com>
2017-05-22 22:00:16 +0530

    validations for posts and edit user profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
     has_many :comments
     has_many :likes
     belongs_to :user
-
+    validates :title, presence: true
     # def self.search(query)
     #   where("desc LIKE ?", "%#{query}%")   
     #  end
